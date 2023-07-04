609e373a80313015bcfc33b346ff0412d18a5692
GoodNightBuddy <fun.love.nuts@gmail.com>
2023-07-04 22:58:14 +0300

    Fixed search res editing/deleting

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Input, InputGroup, InputRightElement } from '@chakra-ui/react';
 import { FaSearch } from 'react-icons/fa';
 import { useAppDispatch, useAppSelector } from '../../store/types/types';
@@ -8,42 +8,46 @@ const Search: React.FC = () => {
   const dispatch = useAppDispatch();
   const { cars } = useAppSelector(state => state.cars);
   const [searchQuery, setSearchQuery] = useState('');
-  const timeoutIdRef = useRef<NodeJS.Timeout | null>(null);
 
-  const handleSearch = (query: string) => {
-    if (query === '') {
-      dispatch(carsActionCreator.removeSearchResult());
-      return;
-    }
+  const handleSearch = useCallback(
+    (query: string) => {
+      if (query === '') {
+        dispatch(carsActionCreator.removeSearchResult());
+        return;
+      }
 
-    const filteredCars = cars.filter(car => {
-      const lowercaseQuery = query.toLowerCase();
-      const carValues = Object.values(car).map(value => {
-        if (typeof value === 'number') {
-          return value.toString();
-        }
-        return value;
+      const filteredCars = cars.filter(car => {
+        const lowercaseQuery = query.toLowerCase();
+        const carValues = Object.values(car).map(value => {
+          if (typeof value === 'number') {
+            return value.toString();
+          }
+          return value;
+        });
+
+        const combinedValues = carValues.join(' ').toLowerCase();
+
+        return combinedValues.includes(lowercaseQuery);
       });
 
-      const combinedValues = carValues.join(' ').toLowerCase();
+      dispatch(carsActionCreator.setSearchResult({ search: filteredCars }));
+    },
+    [dispatch, cars]
+  );
 
-      return combinedValues.includes(lowercaseQuery);
-    });
+  useEffect(() => {
+    const debounceTimeout = setTimeout(() => {
+      handleSearch(searchQuery);
+    }, 500);
 
-    dispatch(carsActionCreator.setSearchResult({ search: filteredCars }));
-  };
+    return () => {
+      clearTimeout(debounceTimeout);
+    };
+  }, [searchQuery, handleSearch]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const query = e.target.value;
     setSearchQuery(query);
-
-    if (timeoutIdRef.current) {
-      clearTimeout(timeoutIdRef.current);
-    }
-
-    timeoutIdRef.current = setTimeout(() => {
-      handleSearch(query);
-    }, 500);
   };
 
   return (
